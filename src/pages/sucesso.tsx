1f348361f2db8343a3943cfc4841fc6b90bfb308
Iago <iagobrazmendes@gmail.com>
2021-02-08 10:19:45 -0300

    create content

--- src/pages/sucesso.tsx
+++ src/pages/sucesso.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import {useRouter} from 'next/router'
 import {useEffect, useState} from 'react'
+import Image from 'next/image'
+import {FiCheckCircle, FiExternalLink} from 'react-icons/fi'
 
 import Container from '../styles/pages/sucesso'
+import logo from '../assets/logo.svg'
 
 const Success: React.FC = () =>
 {
@@ -22,6 +25,28 @@ const Success: React.FC = () =>
 			<Head>
 				<title>Sucesso</title>
 			</Head>
+
+			<header>
+				<div className='img'>
+					<Image src={logo} width={1000} height={350} layout='responsive' />
+				</div>
+				<div className='message'>
+					<FiCheckCircle size={100} />
+					<h1>
+						Sua inscrição foi registrada, mas ainda falta fazer o pagamento para concluí-la!
+					</h1>
+				</div>
+			</header>
+
+			<main>
+				<p>
+					Para realizar operações financeiras, o CEAMONTES decidiu usar o serviço <a target='_blank' rel='noreferrer' href='https://www.widepay.com'>Wide Pay</a>, um sistema de pagamentos confiável e seguro.
+				</p>
+				<a target='_blank' rel='noreferrer' href={link} className='payment'>
+					<span>Fazer pagamento</span>
+					<FiExternalLink size={25} />
+				</a>
+			</main>
 		</Container>
 	)
 }
